1198841b3ccd41fe56b088584ae9af34f07c3d51
puma <2450329248@qq.com>
2019-06-27 23:35:16 +0800

    error

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -78,7 +78,7 @@ return array(
         ),
     ),
     'view_manager' => array(
-        'display_not_found_reason' => true,
+        'display_not_found_reason' => false,
         'display_exceptions'       => false,
         'doctype'                  => 'HTML5',
         'not_found_template'       => 'error/404',
