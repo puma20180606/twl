7591aedecd8b1e1cb1d22e3a020a45f709fd47ed
puma <2450329248@qq.com>
2019-06-27 23:28:28 +0800

    'error_display'

--- public/index.php
+++ public/index.php
@@ -3,8 +3,8 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-ini_set("display_errors", "On");
-error_reporting(E_ALL | E_STRICT);
+ini_set("display_errors", "Off");
+error_reporting(0);
 chdir(dirname(__DIR__));
 define ('ROOT', dirname(__DIR__));
 date_default_timezone_set('Asia/Chongqing');
